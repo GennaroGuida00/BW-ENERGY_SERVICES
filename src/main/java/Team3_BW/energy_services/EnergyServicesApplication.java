64ce0df2645b6ed83c36993881bd18f52b1bbb72
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-08-04 15:26:41 +0200

    initial commit

---
+++ src/main/java/Team3_BW/energy_services/EnergyServicesApplication.java
@@ -0,0 +1,13 @@
+package Team3_BW.energy_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyServicesApplication.class, args);
+	}
+
+}
