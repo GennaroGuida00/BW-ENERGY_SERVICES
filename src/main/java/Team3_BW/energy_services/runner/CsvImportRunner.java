7141654aa0318df24c1fb57359255188d265f4d9
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-08-05 16:24:20 +0200

    created runner

---
+++ src/main/java/Team3_BW/energy_services/runner/CsvImportRunner.java
@@ -0,0 +1,32 @@
+package Team3_BW.energy_services;
+
+import Team3_BW.energy_services.services.ComuneService;
+import Team3_BW.energy_services.services.ProvinciaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CsvImportRunner implements CommandLineRunner {
+
+    @Autowired
+    private ComuneService comuneService;
+    @Autowired
+    private ProvinciaService provinciaService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        String provPath = "src/main/resources/comuni&province/province-italiane.csv";
+        String comuPath = "src/main/resources/comuni&province/comuni-italiani.csv";
+
+        try {
+            //this.provinciaService.importProvinciaFromCsv(provPath);
+            // System.out.println("province imprtate");
+            this.comuneService.importComuniFromCsv(comuPath);
+            System.out.println("done");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+}
