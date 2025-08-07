02b6d50af2301813567e18fe8a9629fcd133644c
Gennaro <gennyguida19@gmail.com>
2025-08-07 17:45:10 +0200

    commit fic filter client and fix service indirizzi

--- src/main/java/Team3_BW/energy_services/payloads/NewIndirizzoDTO.java
+++ src/main/java/Team3_BW/energy_services/payloads/NewIndirizzoDTO.java
@@ -2,6 +2,7 @@ package Team3_BW.energy_services.payloads;
 
 import Team3_BW.energy_services.entities.Comune;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 public record NewIndirizzoDTO (
         @NotEmpty(message = "La via deve essere obbligatoria!")
@@ -11,6 +12,9 @@ public record NewIndirizzoDTO (
         @NotEmpty(message = "La località deve essere obbligatoria!")
         String località,
         @NotEmpty(message = "il cap deve essere obbligatorio!")
-        String cap
+        String cap,
+        @NotNull(message = "Il comune è obbligatorio")
+        long id_comune
+
 ){
 }
--- src/main/java/Team3_BW/energy_services/services/SedeService.java
+++ src/main/java/Team3_BW/energy_services/services/SedeService.java
@@ -1,6 +1,7 @@
 package Team3_BW.energy_services.services;
 
 import Team3_BW.energy_services.entities.Sede;
+import Team3_BW.energy_services.enums.TipoSede;
 import Team3_BW.energy_services.exceptions.NotFoundException;
 import Team3_BW.energy_services.payloads.NewSedeDTO;
 import Team3_BW.energy_services.repositories.SedeRepository;
@@ -29,7 +30,7 @@ public class SedeService {
 
     public Sede add(NewSedeDTO sedeDTO){
         Sede sede=new Sede();
-        sede.setType(sedeDTO.tipoSede());
+        sede.setType(TipoSede.valueOf(sedeDTO.tipoSede()));
        return sedeRepository.save(sede);
 
     }
