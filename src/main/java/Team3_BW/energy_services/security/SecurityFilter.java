636e0b03474845ab1bd827f086050e871abbe823
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-08-07 11:01:22 +0200

    created security config, password encript and filter

---
+++ src/main/java/Team3_BW/energy_services/security/SecurityFilter.java
@@ -0,0 +1,51 @@
+package Team3_BW.energy_services.security;
+
+import Team3_BW.energy_services.entities.Utente;
+import Team3_BW.energy_services.exceptions.UnauthorizedException;
+import Team3_BW.energy_services.services.UtenteService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private JWTTools jwtTools;
+
+    @Autowired
+    private UtenteService utenteService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        if (authHeader == null || !authHeader.startsWith("Bearer "))
+            throw new UnauthorizedException("Inserire token in formato valido");
+        String accessToken = authHeader.replace("Bearer ", "");
+        jwtTools.verifyToken(accessToken);
+
+        String utenteId = jwtTools.extractIdFromToken(accessToken);
+        Utente utenteCorrente = this.utenteService.findById(Long.parseLong(utenteId));
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(utenteCorrente, null, utenteCorrente.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        filterChain.doFilter(request, response);
+    }
+
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return new AntPathMatcher().match("/**", request.getServletPath());
+    }
+}
